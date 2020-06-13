900b58fbe2e64f1828a866be83c8bf35b3ee07fe
Yee Wei Chai <wcyee26@hotmail.com>
2020-06-13 14:45:00 +0800

    Removed root/assets folder, set heading and title.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ for(let x=0; x<consoleTableLines; x++){
 const headingPArray = drawLetterP();
 const headingIArray = drawLetterI();
 const headingNArray = drawLetterN();
+const titleId = "IDENTIFICATION";
+const titleProgram = "PROGRAM";
 
 // Render console table
 const consoleTable = document.getElementById("consoleTable");
@@ -15,15 +17,30 @@ for(let i=0; i<consoleTableArray.length; i++){
     const row = consoleTable.insertRow(i);
     for(let j=0; j<consoleTableLength; j++){
         const cell = row.insertCell(j);
-        let indentLength = 0;
-        if(i >= 0 && i <= headingPArray.length-1){
-            consoleTableArray[i][j] = headingPArray[i][j];
+        if(i >= 0 && i < 6){
+            // Set heading P
+            if(headingPArray[i][j])
+                consoleTableArray[i][j] = headingPArray[i][j];
 
-            indentLength = indentLength + headingPArray.length + 2;
-            consoleTableArray[i][j+indentLength] = headingIArray[i][j];
+            // Set heading I
+            const headingIOffsetIndex = headingPArray[0].length + 2;
+            if(headingIArray[i][j])
+                consoleTableArray[i][j + headingIOffsetIndex] = headingIArray[i][j];
 
-            indentLength = indentLength + headingNArray.length+3;
-            consoleTableArray[i][j+indentLength] = headingNArray[i][j];
+            // Set heading N
+            const headingNOffsetIndex = headingIOffsetIndex + headingIArray[0].length + 3;
+            if(headingNArray[i][j])
+                consoleTableArray[i][j + headingNOffsetIndex] = headingNArray[i][j];
+
+            // Set title IDENTIFICATION
+            const titleIdOffsetIndex = headingNOffsetIndex + headingNArray[0].length + 1;
+            if(titleId && i == 1 && j < titleId.length)
+                consoleTableArray[i][j + titleIdOffsetIndex] = titleId[j];
+
+            // Set title PROGRAM
+            const titleProgramOffsetIndex = headingNOffsetIndex + headingNArray[0].length + 3;
+            if(titleProgram && i == 3 && j < titleProgram.length)
+                consoleTableArray[i][j + titleProgramOffsetIndex] = titleProgram[j];
         }
         if(consoleTableArray[i][j])
             cell.innerHTML = consoleTableArray[i][j];
@@ -38,6 +55,7 @@ function drawLetterP(){
     for(let i=0; i<rowLength; i++){
         array.push([]);
         for(let j=0; j<colLength; j++){
+            array[i][j] = "";
             if((i == 0 || i == 3) && j < colLength-1)
                 array[i][j] = letter;
             if((i == 1 || i == 2) && (j == 0 || j > 3))
@@ -57,6 +75,7 @@ function drawLetterI(){
     for(let i=0; i<rowLength; i++){
         array.push([]);
         for(let j=0; j<colLength; j++){
+            array[i][j] = "";
             if(i == 0 || i == rowLength-1)
                 array[i][j] = letter;
             if(i > 0 && i < rowLength-1 && j == 3)
@@ -74,6 +93,7 @@ function drawLetterN(){
     for(let i=0; i<rowLength; i++){
         array.push([]);
         for(let j=0; j<colLength; j++){
+            array[i][j] = "";
             array[i][0] = letter;
             array[i][colLength-1] = letter;
             if(i > 0 && i < colLength-1)
